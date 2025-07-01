14d92b0f66c5f6821f62e1b80d28123230dbd751
unknown <glimadefreitasgit2002@gmail.com>
2025-07-01 18:07:27 -0300

    Calculando a quantidade de produtos vendios por loja e o ticket medio de cada produto

--- porjeto_automação.py
+++ porjeto_automação.py
@@ -3,14 +3,22 @@ import pandas as pd
 tabela_vendas = pd.read_excel('Vendas.xlsx')
 
 # Visualizar a base de dados
+print("-" *50)
 pd.set_option("display.max_columns", None)
-# print(tabela_vendas)
+print(tabela_vendas)
 
 # Faturamento por loja
+print("-" *50)
 faturamento = tabela_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
 print(faturamento)
+
 # Quantidade de produtos vendidos por loja
+print("-" *50)
+quantidade = tabela_vendas[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
+print(quantidade)
 
 # Ticket medio por produto em cada loja
-
+print("-" *50)
+ticket_medio = (faturamento['Valor Final'] / quantidade['Quantidade']).to_frame()
+print(ticket_medio)
 # Enviar um email com o relatório
