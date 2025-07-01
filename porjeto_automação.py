ad1bf033eb58758119634f81217c9ca8a6cc62df
unknown <glimadefreitasgit2002@gmail.com>
2025-07-01 17:36:24 -0300

    Calculando o faturamento por loja

--- porjeto_automação.py
+++ porjeto_automação.py
@@ -1,12 +1,14 @@
 import pandas as pd
 # Importar a base de dados
-tabela_venda = pd.read_excel('Vendas.xlsx')
+tabela_vendas = pd.read_excel('Vendas.xlsx')
 
 # Visualizar a base de dados
 pd.set_option("display.max_columns", None)
-print(tabela_venda)
-# Faturamento por loja
+# print(tabela_vendas)
 
+# Faturamento por loja
+faturamento = tabela_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
+print(faturamento)
 # Quantidade de produtos vendidos por loja
 
 # Ticket medio por produto em cada loja
