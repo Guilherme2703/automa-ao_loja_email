9c3925187093a115a5e1cc632e4f7c9aca020de6
unknown <glimadefreitasgit2002@gmail.com>
2025-07-01 23:39:12 -0300

    Enviando email

--- porjeto_automação.py
+++ porjeto_automação.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import win32com.client as win32
+
 # Importar a base de dados
 tabela_vendas = pd.read_excel('Vendas.xlsx')
 
@@ -21,4 +23,31 @@ print(quantidade)
 print("-" *50)
 ticket_medio = (faturamento['Valor Final'] / quantidade['Quantidade']).to_frame()
 print(ticket_medio)
+
 # Enviar um email com o relatório
+outlook = win32.Dispatch('outlook.application')
+mail = outlook.CreateItem(0)
+mail.To = 'glimadefreitas20@gmail.com'
+mail.Subject = 'Relatorio de Vendas por Loja'
+mail.HTMLBody = '''
+Prezados,
+
+Segue o Relatório de Vendas por cada Loja.
+
+Faturamento:
+{}
+
+Quantidade Vendida:
+{}
+
+Ticket Médio dos Produtos em cada Loja:
+{}
+
+Qualquer duvida estou a disposição
+
+Att.,
+Guilherme Lima de Freitas
+'''
+mail.Send()
+
+print("Email enviado com sucesso!!!")
